8ea6d6145ae9600dba6ce129d61b304db2e7d51c
Hana Aliyah Mufidah <hanaaliyah@Hanas-MacBook-Air.local>
2025-02-05 15:40:23 +0700

    hardcode contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -19,11 +19,14 @@ contract LendingScript is Script {
         vm.startBroadcast();
 
         // Deploy the mock USDC and WBTC contract
-        MockV3Aggregator usdcAggregator = new MockV3Aggregator(DECIMALS, USDC_USD_PRICE);
-        IERC20 mockUSDC = new MockUSDC();
-
-        MockV3Aggregator wbtcAggregator = new MockV3Aggregator(DECIMALS, WBTC_USD_PRICE);
-        IERC20 mockWBTC = new MockWBTC();
+        // MockV3Aggregator usdcAggregator = new MockV3Aggregator(DECIMALS, USDC_USD_PRICE);
+        // MockV3Aggregator wbtcAggregator = new MockV3Aggregator(DECIMALS, WBTC_USD_PRICE);
+        // IERC20 mockUSDC = new MockUSDC();
+        // IERC20 mockWBTC = new MockWBTC();
+        MockV3Aggregator usdcAggregator = MockV3Aggregator(0x43E328A3CB461426F26FBa0eca129FBb990cbC97);
+        MockV3Aggregator wbtcAggregator = MockV3Aggregator(0x16B7F93876b7Be59C4075017A8fF5A1c6D204304);
+        IERC20 mockUSDC = IERC20(0x849Def4D000EEFC1c3834607ce9F4d9C5b24A670);
+        IERC20 mockWBTC = IERC20(0x9B8918cABf1D4838d988F7d9D459c1ABE3Af3a6D);
 
         // Deploy the LendingPool contract with the address of the mock USDC and WBTC contract
         lendingPool = new LendingPool(mockUSDC, mockWBTC, usdcAggregator, wbtcAggregator);
