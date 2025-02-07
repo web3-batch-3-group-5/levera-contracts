99c3cbf871fc0a1f85da98de0758847029b9baad
Hana Aliyah Mufidah <hanaaliyah@Hanas-MacBook-Air.local>
2025-02-07 16:32:55 +0700

    update event on lending pool factory

--- src/LendingPoolFactory.sol
+++ src/LendingPoolFactory.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {AggregatorV2V3Interface} from "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV2V3Interface.sol";
 import {LendingPool} from "./LendingPool.sol";
 
@@ -23,6 +24,10 @@ contract LendingPoolFactory {
     mapping(bytes32 => PoolParams) public lendingPools;
 
     event AllLendingPool(
+        string loanTokenName,
+        string collateralTokenName,
+        string loanTokenSymbol,
+        string collateralTokenSymbol,
         address loanToken,
         address collateralToken,
         address loanTokenUsdDataFeed,
@@ -44,6 +49,10 @@ contract LendingPoolFactory {
         lendingPools[id] = PoolParams(params, address(lendingPool));
 
         emit AllLendingPool(
+            getTokenName(params.loanToken),
+            getTokenName(params.collateralToken),
+            getTokenSymbol(params.loanToken),
+            getTokenSymbol(params.collateralToken),
             params.loanToken,
             params.collateralToken,
             params.loanTokenUsdDataFeed,
@@ -51,5 +60,21 @@ contract LendingPoolFactory {
             address(lendingPool),
             block.timestamp
         );
+    }
+
+    function getTokenName(address token) internal view returns (string memory) {
+        try ERC20(token).name() returns (string memory tokenName) {
+            return tokenName;
+        } catch {
+            return "Unknown"; // Fallback name if `name()` is not implemented
+        }
+    }
+
+    function getTokenSymbol(address token) internal view returns (string memory) {
+        try ERC20(token).symbol() returns (string memory tokenSymbol) {
+            return tokenSymbol;
+        } catch {
+            return "UNKNOWN"; // Fallback name if `symbol()` is not implemented
+        }
     }
 }
